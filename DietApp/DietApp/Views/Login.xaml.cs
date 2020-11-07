a2a8970742f2b326cd94c935f1e99cd90b18eabf
santosh dahal <exendahal@gmail.com>
2020-11-07 09:06:12 +0545

    Login Page design, Added Font on Android assets,

--- DietApp/DietApp/Views/Login.xaml.cs
+++ DietApp/DietApp/Views/Login.xaml.cs
@@ -12,9 +12,55 @@ namespace DietApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        bool isPassword = true;
         public Login()
         {
+            BindingContext = this;
             InitializeComponent();
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+      
+        private void CustomEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (IsValidEmail(emailAddress.Text))
+            {
+
+                checkIcon.TextColor = Color.FromHex("#61D27C");
+            }
+            else
+            {
+                checkIcon.TextColor = Color.Transparent;
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+           
+           
+            if (isPassword)
+            {
+                isPassword = false;
+                password.IsPassword = isPassword;
+                passSeeIcon.Text = "\uf6d0";
+            }
+            else
+            {
+                isPassword = true;
+                password.IsPassword = isPassword;
+                passSeeIcon.Text = "\uf6cf";
+            }
         }
     }
 }
